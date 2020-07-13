99d2ef584e8a46526fe7c314ebc86b98b0637bc3
lichunyang_18010089 <lichunyang@shinhofood.com>
2020-07-13 15:59:18 +0800

    leetcode lean Arrays

--- src/com/boredream/leetcode/lean/Arrays.java
+++ src/com/boredream/leetcode/lean/Arrays.java
@@ -1,5 +1,7 @@
 package com.boredream.leetcode.lean;
 
+import java.util.HashSet;
+
 /**
  * https://leetcode.com/explore/learn/card/fun-with-arrays
  */
@@ -7,8 +9,7 @@ public class Arrays {
 
     public static void main(String[] args) {
         int[] nums = {0};
-        int[] nums2 = {1};
-        new Arrays().merge(nums, 0, nums2, 1);
+        new Arrays().sortArrayByParity(nums);
         System.out.println(java.util.Arrays.toString(nums));
     }
 
@@ -133,6 +134,108 @@ public class Arrays {
                 nums1[i] = nums1[m--];
             }
         }
+    }
+
+    // 删除所有val，其他数字前移，返回剩余数字长度
+    public int removeElement(int[] nums, int val) {
+        // 记录删除数字，然后后续数字前移删除数量长度
+        int length = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                length++;
+            } else {
+                nums[i - length] = nums[i];
+            }
+        }
+        return nums.length - length;
+    }
+
+    // 有序数组。删除重复数字，非重复的前移，最后返回非重复长度
+    public int removeDuplicates(int[] nums) {
+        // 双指针，low指向非重复到达位置，fast指针遍历全部，和low比较
+        int low = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[low] != nums[i]) {
+                nums[++low] = nums[i];
+            }
+        }
+        return low + 1;
+    }
+
+    // 找到是否有一个数字是另一个2倍
+    public boolean checkIfExist(int[] arr) {
+        // hash
+        HashSet<Integer> set = new HashSet<>();
+        for (int i : arr) {
+            if (set.contains(i * 2) || (i % 2 == 0 && set.contains(i / 2))) return true;
+            set.add(i);
+        }
+        return false;
+    }
+
+    // 小大小，不能有连续相等情况
+    public boolean validMountainArray(int[] A) {
+        if (A == null || A.length < 3) return false;
+        // 双指针，遇到下降停止，最后判断是否同一个点
+        int low = 0;
+        int high = A.length - 1;
+        while (low < high) {
+            boolean leftUp = A[low + 1] > A[low];
+            if (leftUp) low++;
+            boolean rightUp = A[high - 1] > A[high];
+            if (rightUp) high--;
+            if (!leftUp && !rightUp) break;
+        }
+        return low != 0 && high != A.length - 1 && low == high;
+        // TODO 俩while可以分开，不用while里判断break，时间复杂度不变
+    }
+
+    // 每个数字都替换成右边所有数中最大的，最后一个-1
+    public int[] replaceElements(int[] arr) {
+        int max = -1;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int newMax = Math.max(arr[i], max);
+            arr[i] = max;
+            max = newMax;
+        }
+        return arr;
+    }
+
+    // 左右0移到右侧
+    public void moveZeroes(int[] nums) {
+        int numberIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[numberIndex++] = nums[i];
+            }
+        }
+        for (int i = numberIndex; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+    // 先偶数后奇数
+    public int[] sortArrayByParity(int[] A) {
+        // 双指针交换
+        int low = 0;
+        int high = A.length - 1;
+        while (true) {
+            while (low < high && A[low] % 2 == 0) low++;
+            while (low < high && A[high] % 2 == 1) high--;
+            if (low >= high) break;
+            int temp = A[low];
+            A[low] = A[high];
+            A[high] = temp;
+        }
+        return A;
+        // TODO: 2020/7/13 也可以直接冒泡方式，左到右for循环，奇偶交换
+    }
+
+    // 排序前后有多少个数字不同
+    public int heightChecker(int[] heights) {
+        int count = 0;
+
+        return count;
     }
 
 }
