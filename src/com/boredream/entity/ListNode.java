f9a22d3f1c16e3fbaa836daf6527565bbd1a4a78
boredream <48262906@qq.com>
2020-07-21 15:49:59 +0800

    leetcode lean LinkedList

--- src/com/boredream/entity/ListNode.java
+++ src/com/boredream/entity/ListNode.java
@@ -2,37 +2,53 @@ package com.boredream.entity;
 
 
 public class ListNode {
-	public int val;
-	public ListNode next;
+    public int val;
+    public ListNode next;
 
-	public ListNode() {
+    public ListNode() {
 
-	}
+    }
 
-	public ListNode(int x) {
-		val = x;
-	}
+    public ListNode(int x) {
+        val = x;
+    }
 
-	@Override
-	public String toString() {
-		ListNode head = this;
-		StringBuilder sb = new StringBuilder();
-		while (head != null) {
-			sb.append(head.val).append("->");
-			head = head.next;
-		}
-		return sb.toString().substring(0, sb.length() - 2);
-	}
-	
-	public static ListNode array2nodelist(Integer[] array) {
-		ListNode nextNode = null;
-		for (int i = array.length - 1; i >= 0; i--) {
-			ListNode node = new ListNode(array[i]);
-			if (nextNode != null) {
-				node.next = nextNode;
-			}
-			nextNode = node;
-		}
-		return nextNode;
-	}
+    @Override
+    public String toString() {
+        ListNode head = this;
+        if (hasCycle(head)) {
+            return "circle " + head.val;
+        } else {
+            StringBuilder sb = new StringBuilder();
+            while (head != null) {
+                sb.append(head.val).append("->");
+                head = head.next;
+            }
+            return sb.toString().substring(0, sb.length() - 2);
+        }
+    }
+
+    public static ListNode array2nodelist(Integer[] array) {
+        ListNode nextNode = null;
+        for (int i = array.length - 1; i >= 0; i--) {
+            ListNode node = new ListNode(array[i]);
+            if (nextNode != null) {
+                node.next = nextNode;
+            }
+            nextNode = node;
+        }
+        return nextNode;
+    }
+
+    public boolean hasCycle(ListNode head) {
+        // 快慢指针，如果追的上就代表有循环
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
 }
--- src/com/boredream/leetcode/lean/LinkedListTest.java
+++ src/com/boredream/leetcode/lean/LinkedListTest.java
@@ -1,130 +1,51 @@
 package com.boredream.leetcode.lean;
 
+import com.boredream.entity.ListNode;
+
 /**
  * https://leetcode.com/explore/learn/card/linked-list
  */
 public class LinkedListTest {
 
     public static void main(String[] args) {
-        LinkedListTest linkedList = new LinkedListTest(); // Initialize empty LinkedList
-        System.out.println(linkedList);
-        linkedList.addAtHead(1);
-        System.out.println(linkedList);
-        linkedList.addAtTail(3);
-        System.out.println(linkedList);
-        linkedList.addAtIndex(1, 2);  // linked list becomes 1->2->3
-        System.out.println(linkedList);
-        int result = linkedList.get(1);// returns 2
-        System.out.println(result);
-        linkedList.deleteAtIndex(1);  // now the linked list is 1->3
-        System.out.println(linkedList);
-        result = linkedList.get(1);     // returns 3
-        System.out.println(result);
+        ListNode node = ListNode.array2nodelist(new Integer[]{3});
+        System.out.println(new LinkedListTest().detectCycle(node));
     }
 
-    private class MyNode {
-        public int val;
-        public MyNode next;
-
-        public MyNode(int val) {
-            this.val = val;
+    // 判断是否有循环
+    public boolean hasCycle(ListNode head) {
+        // 快慢指针，如果追的上就代表有循环
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow) return true;
         }
+        return false;
     }
 
-    public MyNode head;
-    public int length = 0;
-
-    /**
-     * Initialize your data structure here.
-     */
-    public LinkedListTest() {
-
-    }
-
-    /**
-     * Get the value of the index-th node in the linked list. If the index is invalid, return -1.
-     */
-    public int get(int index) {
-        if (index < 0 || index >= length) return -1;
-
-        MyNode cur = head;
-        for (int i = 0; i < index; i++) {
-            cur = cur.next;
+    // 找到循环开始点
+    public ListNode detectCycle(ListNode head) {
+        // 快慢指针
+        // 找到重合点后，fast比slow多跑一个循环
+        // 开始到循环起点A，循环起点到交汇点B，交汇点循环到起点为C
+        // 所以第一次交汇 slow=A+B，fast=A+B+C，又因为速度是两倍 2(A+B)=A+B+C，得到A=C
+        // 因此在第一次交汇后，重新让一个点从开始，另一个店从交汇处，都前进A=C就会一起到达B点
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow) break;
         }
-        return cur.val;
-    }
-
-    /**
-     * Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
-     */
-    public void addAtHead(int val) {
-        MyNode oldHead = head;
-        if (head == null) {
-            head = new MyNode(val);
+        if(fast == null || fast.next == null) return null;
+        slow = head;
+        while(slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
         }
-        head.next = oldHead;
-        length++;
+        return fast;
     }
 
-    /**
-     * Append a node of value val to the last element of the linked list.
-     */
-    public void addAtTail(int val) {
-        if (head == null) {
-            head = new MyNode(val);
-        } else {
-            head.next = new MyNode(val);
-        }
-        length++;
-    }
-
-    /**
-     * Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
-     */
-    public void addAtIndex(int index, int val) {
-        if (index < 0 || index > length) return;
-
-        if (head == null) {
-            head = new MyNode(val);
-        } else {
-            MyNode pre = head;
-            MyNode cur = head.next;
-            for (int i = 1; i < index; i++) {
-                pre = pre.next;
-                cur = cur.next;
-            }
-            MyNode node = new MyNode(val);
-            pre.next = node;
-            node.next = cur;
-        }
-        length++;
-    }
-
-    /**
-     * Delete the index-th node in the linked list, if the index is valid.
-     */
-    public void deleteAtIndex(int index) {
-        if (index < 0 || index > length || length == 0) return;
-
-        MyNode pre = head;
-        for (int i = 0; i < index - 1; i++) {
-            pre = pre.next;
-        }
-        MyNode cur = pre.next;
-        pre.next = cur.next;
-        cur.next = null;
-
-        length--;
-    }
-
-    @Override
-    public String toString() {
-        MyNode cur = head;
-        StringBuilder sb = new StringBuilder();
-        while (cur != null) {
-            sb.append(cur.val).append(",");
-            cur = cur.next;
-        }
-        return sb.length() == 0 ? "null" : sb.deleteCharAt(sb.length() - 1).toString();
-    }
 }
