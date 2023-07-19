a32b140a1177ca380a799384c5120de55be8d8ba
boredream <48262906@qq.com>
2023-07-19 10:51:20 +0800

    Q6

--- src/com/boredream/leetcode/Q6Zconvert.java
+++ src/com/boredream/leetcode/Q6Zconvert.java
@@ -14,14 +14,39 @@ public class Q6Zconvert {
         System.out.println(convert("LEETCODEISHIRING", 4));
     }
 
+    static String convert1(String s, int numRows) {
+        // 思路：找规律，第一行是波峰，隔 2*numRows-2；
+        // 第二行对于每个波峰都 index-1 index+1 各一个；
+        // 第三行和第二行同理，也是 index-2 index+2
+        if (numRows <= 0) return null;
+        if (s == null || s.length() == 0) return s;
+        if (numRows == 1 || numRows >= s.length()) return s;
+
+        StringBuilder sb = new StringBuilder();
+        int step = 2 * numRows - 2;
+        for (int row = 0; row < numRows; row++) {
+            for (int i = row; i < s.length(); i += step) {
+                sb.append(s.charAt(i));
+                // 中间行，还要找下个中间数
+                if(row > 0 && row < numRows - 1) {
+                    int mid = i + (numRows - row - 1) * 2;
+                    if(mid < s.length()) {
+                        sb.append(s.charAt(mid));
+                    }
+                }
+            }
+        }
+        return sb.toString();
+    }
+
     static String convert(String s, int numRows) {
         // 思路1：找规律的题目。n行，则下行n个数，回升n-2个数字，然后继续循环下行
         // 所以首行数字应该是 index % (n + n - 2) == 0
         // 次行数字 index % (n + n - 2) == 1 加上 这后面一个上行数字
 
-        if(numRows <= 0) return null;
-        if(s == null || s.length() == 0) return s;
-        if(numRows == 1 || numRows >= s.length()) return s;
+        if (numRows <= 0) return null;
+        if (s == null || s.length() == 0) return s;
+        if (numRows == 1 || numRows >= s.length()) return s;
 
         StringBuilder sb = new StringBuilder();
         for (int level = 0; level < numRows; level++) {
