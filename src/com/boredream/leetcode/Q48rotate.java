9919553fd3e522e6b17c2d7f86da86e0391d3794
boredream <48262906@qq.com>
2023-07-26 11:58:51 +0800

    Q48

--- src/com/boredream/leetcode/Q48rotate.java
+++ src/com/boredream/leetcode/Q48rotate.java
@@ -31,16 +31,32 @@ public class Q48rotate {
 
     public static void main(String[] args) {
         int[][] matrix = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
+                {2, 29, 20, 26, 16, 28},
+                {12, 27, 9, 25, 13, 21},
+                {32, 33, 32, 2, 28, 14},
+                {13, 14, 32, 27, 22, 26},
+                {33, 1, 20, 7, 21, 7},
+                {4, 24, 1, 6, 32, 34}
         };
         rotate(matrix);
         System.out.println(Arrays.deepToString(matrix));
     }
 
     static void rotate(int[][] matrix) {
-
+        // 思路：按圈逐层分为多个正方层，每次单层每个边进行4次轮询替换
+        int n = matrix.length;
+        int circleCount = n / 2; // 不用+1 因为奇数中心圈就一个数字不用旋转
+        for (int i = 0; i < circleCount; i++) {
+            // 边长-1个数字
+            for (int j = 0; j < n - i * 2 - 1; j++) {
+                // 4个边挨个替换
+                int temp = matrix[i][i + j];
+                matrix[i][i + j] = matrix[n - 1 - i - j][i];
+                matrix[n - 1 - i - j][i] = matrix[n - 1 - i][n - 1 - i - j];
+                matrix[n - 1 - i][n - 1 - i - j] = matrix[i + j][n - 1 - i];
+                matrix[i + j][n - 1 - i] = temp;
+            }
+        }
     }
 
 }
