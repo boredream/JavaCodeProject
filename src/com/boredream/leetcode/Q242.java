f5b992f8899909cb121778b91683c4281e32e04b
boredream <48262906@qq.com>
2023-07-28 11:10:43 +0800

    Q242

---
+++ src/com/boredream/leetcode/Q242.java
@@ -0,0 +1,49 @@
+package com.boredream.leetcode;
+
+/**
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ * <p>
+ * 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ * <p>
+ * <p>
+ * 提示:
+ * <p>
+ * 1 <= s.length, t.length <= 5 * 104
+ * s 和 t 仅包含小写字母
+ * <p>
+ * <p>
+ * 进阶: 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+ */
+public class Q242 {
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("rat", "car"));
+    }
+
+    static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            if (--count[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
