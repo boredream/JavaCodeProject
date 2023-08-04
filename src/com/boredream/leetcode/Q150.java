aefe733afbec78618e3d8e14ac50d281448d3f25
boredream <48262906@qq.com>
2023-08-04 09:35:13 +0800

    Q150

--- src/com/boredream/leetcode/Q150.java
+++ src/com/boredream/leetcode/Q150.java
@@ -41,7 +41,7 @@ import java.util.Stack;
 public class Q150 {
 
     public static void main(String[] args) {
-        System.out.println(evalRPN(new String[]{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}));
+        System.out.println(evalRPN1(new String[]{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}));
     }
 
     static int evalRPN(String[] tokens) {
@@ -68,6 +68,28 @@ public class Q150 {
                 default:
                     stack.push(Integer.parseInt(token));
                     break;
+            }
+        }
+        return stack.pop();
+    }
+
+    static int evalRPN1(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            if ("+".equals(token)) {
+                stack.push(stack.pop() + stack.pop());
+            } else if ("*".equals(token)) {
+                stack.push(stack.pop() * stack.pop());
+            } else if ("-".equals(token)) {
+                Integer second = stack.pop();
+                Integer first = stack.pop();
+                stack.push(first - second);
+            } else if ("/".equals(token)) {
+                Integer second = stack.pop();
+                Integer first = stack.pop();
+                stack.push(first / second);
+            } else {
+                stack.push(Integer.parseInt(token));
             }
         }
         return stack.pop();
