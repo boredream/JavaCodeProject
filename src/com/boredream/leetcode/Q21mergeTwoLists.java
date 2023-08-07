f3733b86628d91bd0e80814707f79ce1cf930dbe
boredream <48262906@qq.com>
2023-08-07 15:22:59 +0800

    Q21

--- src/com/boredream/leetcode/Q21mergeTwoLists.java
+++ src/com/boredream/leetcode/Q21mergeTwoLists.java
@@ -7,7 +7,8 @@ public class Q21mergeTwoLists {
     public static void main(String[] args) {
         ListNode l1 = ListNode.array2nodelist(new Integer[]{1, 2, 4});
         ListNode l2 = ListNode.array2nodelist(new Integer[]{1, 3, 4});
-        System.out.println(mergeTwoLists(l1, l2));
+//        System.out.println(mergeTwoLists(l1, l2));
+        System.out.println(mergeTwoLists2(l1, l2));
     }
 
     static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
@@ -38,4 +39,25 @@ public class Q21mergeTwoLists {
         return node.next;
     }
 
+    static ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        // 思路：俩数据对比，谁小先排谁
+        ListNode node = new ListNode();
+        ListNode cur = node;
+        while(list1 != null && list2 != null) {
+            if(list1.val <= list2.val) {
+                cur.next = new ListNode(list1.val);
+                list1 = list1.next;
+            } else {
+                cur.next = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        if(list1 == null) {
+            cur.next = list2;
+        } else {
+            cur.next = list1;
+        }
+        return node.next;
+    }
 }
