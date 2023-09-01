5631b97019d5bfdde236305edeaa72a262c2d743
boredream <48262906@qq.com>
2023-09-01 12:03:03 +0800

    Q17

--- src/com/boredream/leetcode/Q17letterCombinations.java
+++ src/com/boredream/leetcode/Q17letterCombinations.java
@@ -15,7 +15,7 @@ public class Q17letterCombinations {
 
     public static void main(String[] args) {
         System.out.println(letterCombinations("47"));
-        System.out.println(letterCombinations2(""));
+        System.out.println(letterCombinations3("47"));
     }
 
     static List<String> letterCombinations(String digits) {
@@ -23,7 +23,7 @@ public class Q17letterCombinations {
         // 其实是一个2-abc 套3-def的3*3=9结果三层循环，但for嵌套数量不确定，所以~ 要用到经典的回溯法
         // 为了方便理解可以转为，3*3的树，然后用前序排列探出每个叶节点，用递归
         List<String> list = new ArrayList<>();
-        if(digits == null || digits.length() == 0) return list;
+        if (digits == null || digits.length() == 0) return list;
         step(list, "", digits);
         return list;
     }
@@ -55,7 +55,7 @@ public class Q17letterCombinations {
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
             String newPre = pre + c;
-            if(digits.length() - pre.length() == 1) {
+            if (digits.length() - pre.length() == 1) {
                 list.add(newPre);
                 continue;
             }
@@ -66,7 +66,24 @@ public class Q17letterCombinations {
     // TODO: chunyang 2023/9/1 普通递归+pre方式，会有多个pre变量，所以内存消耗较大。
     //  要考虑使用回溯，一个StringBuilder，add到叶节点后，再delete回溯到下一个情况
 
+    static List<String> letterCombinations3(String digits) {
+        // 思路：回溯
+        List<String> list = new ArrayList<>();
+        backtrack(list, new StringBuilder(), digits);
+        return list;
+    }
 
-
+    private static void backtrack(List<String> list, StringBuilder sb, String digits) {
+        if (sb.length() == digits.length()) {
+            list.add(sb.toString());
+            return;
+        }
+        String str = charmap[digits.charAt(sb.length()) - '0'];
+        for (char c : str.toCharArray()) {
+            sb.append(c);
+            backtrack(list, sb, digits);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
 
 }
